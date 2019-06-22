33ff21f3401f67769af30dbbf07e4665034501e2
Patrick.Wilke <patrick.wilke@physik.uni-muenchen.de>
2019-06-22 22:17:22 +0200

    first version commit

---
+++ TicTacToeBoard.py
@@ -0,0 +1,90 @@
+import numpy as np
+import ANN
+import tensorflow as tf
+
+class TicTacToeBoard:
+
+    def SiteIsOccupied(self, row, column):
+        return self.positions[0, row, column] or self.positions[1, row, column]
+
+
+    def WinningMove(self,row, column):
+        if self.positions[self.next_move, 0, column] and self.positions[self.next_move, 1, column] and self.positions[self.next_move, 2, column]:
+            return True
+        if self.positions[self.next_move, row, 0] and self.positions[self.next_move, row, 1] and self.positions[self.next_move, row, 2]:
+            return True
+        if row == column and self.positions[self.next_move, 0, 0] and self.positions[self.next_move, 1, 1] and self.positions[self.next_move, 2, 2]:
+            return True
+        if row == 2-column and self.positions[self.next_move, 0, 2] and self.positions[self.next_move, 1, 1] and self.positions[self.next_move, 2, 0]:
+            return True
+        return False
+
+    def ResetGame(self):
+        self.number_of_moves = 0
+        self.positions = np.zeros((2, 3, 3), dtype=bool)
+        self.next_move = 0
+
+    def __init__(self):
+        self.positions = np.zeros((2, 3, 3), dtype=bool)
+        self.next_move = 0
+        self.number_of_moves = 0
+
+    def GameEnded(self):
+        return self.number_of_moves == 9
+
+    def GetNextMove(self):
+        return self.next_move
+
+    def MakeMove(self,row, column):
+        self.number_of_moves += 1
+        self.positions[self.next_move, row, column] = True
+        if self.WinningMove(row, column):
+            return True
+        else:
+            self.next_move = 1 - self.next_move
+            return False
+
+    def GetSate(self):
+        return self.positions.astype(float).flatten()
+
+    def __str__(self):
+        state = "-------------\n"
+        for i in range(0,3):
+            state += "|"
+            for j in range(0, 3):
+                if self.positions[0, i, j]:
+                    state += " X |"
+                elif self.positions[1, i, j]:
+                    state += " O |"
+                else:
+                    state += "   |"
+            state += "\n-------------\n"
+        return state
+
+class LearningBoard(TicTacToeBoard):
+
+    winning_action_reward = 1.0
+    loosing_action_reward = -0.5
+    neutral_action_reward = 0.0
+    prohibited_action_reward = -1.0
+
+
+    def MakeMoveWithReward(self, action):
+        row = action // 3
+        column = action % 3
+
+        if self.SiteIsOccupied(row,column):
+            return self.prohibited_action_reward, True
+        elif self.MakeMove(row,column):
+            return self.winning_action_reward, True
+        else:
+            if self.GameEnded():
+                return self.neutral_action_reward, True
+            return self.neutral_action_reward, False
+
+
+store_path = "./TicTacToeNewTraining"
+
+if __name__ == '__main__':
+    board = LearningBoard()
+    ANN.SARSA_Training(board,store_path,1000)
---
+++ TicTacToeGUI.py
@@ -0,0 +1,110 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import (
+    ListProperty, StringProperty
+)
+import TicTacToeBoard as TTTB
+import ANN
+
+
+load_path = "./TicTacToeEasy"
+AI_for_X = False
+AI_for_O = True
+
+class TicTacToeGame(Widget):
+
+    board = TTTB.TicTacToeBoard()
+
+    fields = ListProperty(["","","","","","","","",""])
+
+    display_score_X = StringProperty("")
+    display_score_O = StringProperty("")
+
+
+    game_ended = False
+    show_score = False
+
+    score = [0,0]
+
+
+    def SetField(self, row, column):
+        if self.board.GetNextMove() == 0:
+            self.fields[3*row + column] = "X"
+        else:
+            self.fields[3 * row + column] = "O"
+
+    def ShowScoreAfterEnd(self):
+        self.board.ResetGame()
+        self.fields = ["", "", "", "", "", "", "", "", ""]
+        self.game_ended = False
+        self.show_score = True
+        self.display_score_X = "X:" + str(self.score[0])
+        self.display_score_O = "O:" + str(self.score[1])
+
+    def RestartGameAfterScore(self):
+        self.show_score = False
+        self.display_score_X = ""
+        self.display_score_O = ""
+        if AI_for_X and not AI_for_O:
+            self.AIMakeMove()
+
+    def NormalGameMove(self, row, column):
+
+        if not self.board.SiteIsOccupied(row, column):
+            self.SetField(row, column)
+            if self.board.MakeMove(row, column):
+                self.score[self.board.GetNextMove()] += 1
+                self.game_ended = True
+
+            elif self.board.GameEnded():
+                self.game_ended = True
+
+    def AIMakeMove(self):
+        state = self.board.GetSate()
+        action = ANN.GetOptimalAction(state)
+        if self.board.SiteIsOccupied(action // 3, action % 3):
+            print("AI made prohibited move. Game counts as Draw.")
+            self.game_ended = True
+        else:
+            self.NormalGameMove(action//3,action%3)
+
+
+    def on_touch_down(self, touch):
+        if self.game_ended:
+            self.ShowScoreAfterEnd()
+        elif self.show_score:
+            self.RestartGameAfterScore()
+        else:
+
+            if not (AI_for_O and AI_for_X):
+                row = int(2 - (3 * touch.y) // self.top)
+                column = int((3 * touch.x) // self.width)
+                self.NormalGameMove(row, column)
+            if AI_for_X and not self.game_ended:
+                self.AIMakeMove()
+            if AI_for_O and not self.game_ended:
+                self.AIMakeMove()
+    pass
+
+
+class TicTacToeApp(App):
+    def build(self):
+        game = TicTacToeGame()
+        if AI_for_X and not AI_for_O:
+            game.AIMakeMove()
+        return game
+
+
+if __name__ == '__main__':
+    if AI_for_X or AI_for_O:
+        with ANN.tf.Session() as sess:
+            saver = ANN.tf.train.Saver()
+            if ANN.os.path.isfile(load_path + ".index"):
+                saver.restore(sess, load_path)
+            else:
+                print("Path for AI does not exist! Game with completely untrained AI ...")
+                ANN.init.run()
+            TicTacToeApp().run()
+    else:
+        TicTacToeApp().run()
+
